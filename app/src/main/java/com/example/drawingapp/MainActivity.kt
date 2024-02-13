af014e3abe97e643626cd7da5c8ddaa96fb278b2
DylanQuach <43456797+DylanQuach@users.noreply.github.com>
2024-02-12 17:11:14 -0700

    intializing Project

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.drawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
