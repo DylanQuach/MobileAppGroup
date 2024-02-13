5a69d8afc5104a2139f3661b67952fba9b918aaf
Emiliano Plaza <emilianoplaza77@gmail.com>
2024-02-13 15:42:31 -0700

    fixed bug in MainActivity and created BrushViewModel

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drawingapp.databinding.ActivityMainBinding
 
-val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
-
-//val recycler by lazy{ binding.settingRecycler }
-
 class MainActivity : AppCompatActivity() {
+
+    val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    val recycler by lazy{ binding.settingRecycler }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
