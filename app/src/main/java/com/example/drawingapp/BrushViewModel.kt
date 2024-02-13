411daa3f2761fb76dd932fac18f5ec1e81d9f752
Emiliano Plaza <emilianoplaza77@gmail.com>
2024-02-13 15:48:14 -0700

    added some methods to ViewModel

--- app/src/main/java/com/example/drawingapp/BrushViewModel.kt
+++ app/src/main/java/com/example/drawingapp/BrushViewModel.kt
@@ -1,6 +1,39 @@
 package com.example.drawingapp
 
-class BrushViewModel {
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
 
+class BrushViewModel : ViewModel(){
+
+    //Model
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val _color : MutableLiveData<Color> =
+        MutableLiveData(Color.valueOf(1f, 1f, 0f))
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    val color  = _color as LiveData<Color>
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun pickColor(){
+        with(Random.Default) {
+            _color.value = Color.valueOf(nextFloat(), nextFloat(), nextFloat())
+        }
+    }
+    
+    fun pickBrush(){
+
+    }
+    fun pickBrushSize(){
+
+    }
+
+    fun doSomething(){
+
+    }
 
 }
