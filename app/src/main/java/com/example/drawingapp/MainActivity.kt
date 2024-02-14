1d5fd55027c292c7a5aadca1bae1d7ddb62e0452
DylanQuach <43456797+DylanQuach@users.noreply.github.com>
2024-02-13 23:36:21 -0700

    added navbar and setting templates

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,13 +4,39 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drawingapp.databinding.ActivityMainBinding
 
-val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
-
 //val recycler by lazy{ binding.settingRecycler }
 
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectFragment = SelectSettingFragment()
+        selectFragment.setListener {
+            if (it == "color") {
+                val selectedFrag = ColorFragment()
+                val fTrans = supportFragmentManager.beginTransaction()
+                fTrans.replace(R.id.settingFragment, selectedFrag)
+                fTrans.commit()
+            }
+            else if (it == "brush") {
+                val selectedFrag = BrushFragment()
+                val fTrans = supportFragmentManager.beginTransaction()
+                fTrans.replace(R.id.settingFragment, selectedFrag)
+                fTrans.commit()
+            }
+            else if (it == "size") {
+                val selectedFrag = SizeFragment()
+                val fTrans = supportFragmentManager.beginTransaction()
+                fTrans.replace(R.id.settingFragment, selectedFrag)
+                fTrans.commit()
+            }
+        }
+        val fTrans = supportFragmentManager.beginTransaction()
+        fTrans.replace(R.id.selectSettingFragmentView, selectFragment)
+        fTrans.commit()
+
     }
 }
