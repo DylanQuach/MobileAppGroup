f3c586b731297f162b7f44ba8588f6b475a4a28f
Noah <noahhguidry@gmail.com>
2024-02-16 13:14:22 -0700

    back button summons the wheel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation ("androidx.activity:activity-ktx:1.8.2")
     implementation ("androidx.fragment:fragment-ktx:1.6.2")
 
+    implementation("com.github.yukuku:ambilwarna:2.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.drawingapp
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.drawingapp.databinding.ActivityMainBinding
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+//import com.example.drawingapp.databinding.ActivityMainBinding
+import yuku.ambilwarna.AmbilWarnaDialog
 
 
 class MainActivity : AppCompatActivity() {
 
-    val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
-
+    //val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    var color = 1
+    lateinit var layout: ConstraintLayout
+    lateinit var colorBtn: Button
 //    val recycler by lazy{ binding.settingRecycler }
 
 
@@ -23,6 +29,9 @@ class MainActivity : AppCompatActivity() {
 //            startActivity(gotoSplashScreen)
 //            hasSeenSplash = true
 //        }
+
+
+
 
         // Emi code start
         val drawFragment = canvasFragment()
@@ -57,5 +66,37 @@ class MainActivity : AppCompatActivity() {
         val fTrans = supportFragmentManager.beginTransaction()
         fTrans.replace(R.id.selectSettingFragmentView, selectFragment)
         fTrans.commit()
+
+
+        colorBtn = findViewById<Button>(R.id.backButton)
+        //layout = findViewById<ConstraintLayout>(R.id.mainLayout)
+
+        color = ContextCompat.getColor(this@MainActivity, com.google.android.material.R.color.design_default_color_primary)
+
+        colorBtn.setOnClickListener{
+            openColorPicker()
+        }
+
+    }
+
+    /**
+     * displays the dialog box containing the color selection tool. upon closing
+     * the dialog window the color will be selected and the colorDisplay will
+     * be updated
+     */
+    public fun openColorPicker() {
+
+        val ambiListener = object : AmbilWarnaDialog.OnAmbilWarnaListener{
+            override fun onCancel(dialog: AmbilWarnaDialog?) {}
+
+            override fun onOk(dialog: AmbilWarnaDialog?, color: Int) {
+                this@MainActivity.color = color
+                findViewById<View>(R.id.colorButton).setBackgroundColor(this@MainActivity.color)
+            }
+        }
+        val warn = AmbilWarnaDialog(this, color, ambiListener)
+
+        warn.show()
+
     }
 }
